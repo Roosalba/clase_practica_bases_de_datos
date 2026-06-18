7d0b0fb188bf0a25fd552396b6a245009a6dcabd
Rosalba <rosalbagarciaosorio151998@gmail.com>
2026-06-18 10:52:17 -0300

    productos guardados

---
+++ consulta_con_delete.py
@@ -0,0 +1,24 @@
+from conexion_db import obtener_conexion
+
+
+conexion=obtener_conexion()
+cursor=conexion.commit()
+
+# DELETE FROM nombre_de_tabla WHERE condicion;
+
+
+'''
+para eliminar un producto en mysql tenemos que hacer lo siguiente 
+creamos una variable y le colocamos el nombre de lo que queremos 
+eliminar de la siguiente manera nombre_producto="lapiz", se tiene
+que escribir como lo tenemos en nuestra bases de datos, usamos el 
+cursor que es el que tiene contacto con la base de datos y eso se hace
+de la siguiente manera 
+cursor.execute("DELETE FROM productos WHERE nombre = ?",(nombre_producto,))
+la coma es que nos devuelve una tupla
+NOTA: siempre cuando hacemos un delete, uptade, un insert nuestra conexion
+termina en conexion.commiit() -> esto quiero decir que guardo los cambios
+
+
+
+'''
